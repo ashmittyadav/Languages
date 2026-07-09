5aba3f0ffd174b2e460f06090499d721d552ba86
ashister <ashmityadavat92@gmail.com>
2026-07-09 20:52:33 +0530

    Learn C language concepts

---
+++ C/01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("enter any number");
+    scanf("%d",&num);
+    printf("factorial of %d is %d",num,num;
+    return 0;
+}
+// int_fact(n){
+//     if(n==1 || n==0){
+//         return 1;
+//     }
+//     else{
+//         return n*fact(n-1);
+//     }
+// }
---
+++ C/kyu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("wni");
+    scanf("%d",&n);
+    printf(n);
+    return 0;
+}
