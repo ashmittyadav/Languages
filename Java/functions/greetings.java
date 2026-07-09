a45e4a1617892af3821228ad5e716653d792f32a
Ashmit Yadav <ashmityadavat92@gmail.com>
2026-07-09 21:40:33 +0530

    Delete Java directory

--- Java/functions/greetings.java
+++
@@ -1,9 +0,0 @@
-// import java.util.Scanner;
-public class greetings {
-    public static void main(String[] args) {
-     greet();   
-    }
-    static void greet(){
-        System.out.println("hello world");
-    }
-}
