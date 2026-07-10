d98a0b0ef8a9cde61ace680dc87236d44c5837ff
ashister <ashmityadavat92@gmail.com>
2026-07-10 22:52:04 +0530

    Java Mixed Pattern

---
+++ Java/Programs/mixed_pattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class first
+{
+    public static void main(String args[])
+    {
+        int i,j,a,k;
+        Scanner in=new Scanner(System.in);
+        a=in.nextInt();
+        for(i=0;i<a;i++){
+             for(k=3;k>=i;k--){
+                System.out.print(" ");
+             }
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+           
+            }
+           // System.out.println(" ");
+        }
+    
