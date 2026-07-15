01497cec72c759e475cfb57298056f091ed1d25e
ashister <ashmityadavat92@gmail.com>
2026-07-15 23:09:16 +0530

    Add Star pattern-2 implementation

---
+++ Java/Programs/star2_pattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class first
+{
+    public static void main(String args[])
+    {
+        int i,j,a,k;
+        Scanner in=new Scanner(System.in);
+        a=in.nextInt();
+        for(i=0;i<=a;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(k=1;k<=a-i;k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+         for(i=1;i<=a;i++){
+            for(j=1;j<=a-i;j++){
+                System.out.print(" ");
+            }
+            for(k=1;k<=i;k++){
+                System.out.print("*");
+    }
+     System.out.println(" ");
+}
+    }
+}
